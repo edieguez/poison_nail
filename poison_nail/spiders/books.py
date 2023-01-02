aa8f28bb8ac8a869a67090e0cdde8a9e4fa6559f
edieguez <dieguezemmanuel@gmail.com>
2023-01-01 23:07:26 -0500

    Changed default spider for crawler

--- poison_nail/spiders/books.py
+++ poison_nail/spiders/books.py
@@ -1,22 +1,21 @@
-import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 
-class BooksSpider(scrapy.Spider):
+class BooksSpider(CrawlSpider):
     name = 'books'
     allowed_domains = ['books.toscrape.com']
     start_urls = ['http://books.toscrape.com']
+    rules = (
+        Rule(LinkExtractor(allow=r'catalogue/page-\d+\.html'), callback='parse_item', follow=True),
+    )
 
-    def parse(self, response):
+    def parse_item(self, response):
         books = response.css('article.product_pod')
 
         for book in books:
             yield {
                 'name': book.css('h3 a::attr(title)').get(),
-                'price' : book.css('div.product_price p.price_color::text').get(),
-                'image' : response.urljoin(book.css('div.image_container img::attr(src)').get()),
+                'price': book.css('div.product_price p.price_color::text').get(),
+                'image': response.urljoin(book.css('div.image_container img::attr(src)').get()),
             }
-
-        next_page = response.css('li.next a::attr(href)').get()
-
-        if next_page:
-            yield response.follow(next_page, callback=self.parse)
